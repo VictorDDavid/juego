9483e23969c100f16cbf315e6d8d00b05e41d60e
Diurno <Diurno@C08PC06.informatica.comercio>
2024-03-07 12:32:41 +0100

    primera prueba con la clase Run

---
+++ src/main/java/ejercicios/game/Run.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicios.game;
+
+/**
+ *
+ * @author Diurno
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
